33ffa0a3ec7a8316bf34ca3979a6c2128461d980
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-28 18:40:20 +0000

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import numpy as np
 
 radii = np.loadtxt("bubbles.dat")
 
-print("The smallest bubble has a radius of")
-print("25% of the bubbles have a radius that is less than or equal to")
-print("The median radius of the bubbles is")
-print("75% of the bubbles have a radius that is less than or equal to ")
-print("The largest bubble has a radius of")
+print("The smallest bubble has a radius of", dmin)
+print("25% of the bubbles have a radius that is less than or equal to", lowq)
+print("The median radius of the bubbles is", median)
+print("75% of the bubbles have a radius that is less than or equal to", highq)
+print("The largest bubble has a radius of", dmax)
