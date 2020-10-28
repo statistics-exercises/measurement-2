d5be53fcdb04f09c4bbc1ffdd6cfa253c4f3c9e8
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-28 18:41:39 +0000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -10,9 +10,9 @@ class UnitTests(unittest.TestCase) :
         self.assertTrue( "dmax" in globals(), "The variable dmax is not defined" )
         
    def test_values(self) : 
-       x.sort()
-       self.assertTrue( x[0]==dmin, "dmin has not been calculated correctly" )
-       self.assertTrue( x[len(x)-1]==dmax, "dmax has not been calculated correctly" )
-       self.assertTrue( np.abs( np.percentile( x, 25 ) - lowq ) < 1e-7, "lowq has not been calculated correctly" )
-       self.assertTrue( np.abs( np.percentile( x, 50) - median ) < 1e-7, "median has not been calculated correctly" )
-       self.assertTrue( np.abs( np.percentile(x, 75 ) - highq ) < 1e-7, "highq has not been calculated correctly" )
+       radii.sort()
+       self.assertTrue( radii[0]==dmin, "dmin has not been calculated correctly" )
+       self.assertTrue( radii[len(x)-1]==dmax, "dmax has not been calculated correctly" )
+       self.assertTrue( np.abs( np.percentile( radii, 25 ) - lowq ) < 1e-7, "lowq has not been calculated correctly" )
+       self.assertTrue( np.abs( np.percentile( radii, 50) - median ) < 1e-7, "median has not been calculated correctly" )
+       self.assertTrue( np.abs( np.percentile( radii, 75 ) - highq ) < 1e-7, "highq has not been calculated correctly" )
