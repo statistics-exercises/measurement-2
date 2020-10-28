d55f95012eeb8c78dc9ebbaca54accc2874c726b
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-28 18:34:19 +0000

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+radii = np.loadtxt("bubbles.dat")
+
+print("The smallest bubble has a radius of")
+print("25% of the bubbles have a radius that is less than or equal to")
+print("The median radius of the bubbles is")
+print("75% of the bubbles have a radius that is less than or equal to ")
+print("The largest bubble has a radius of")
